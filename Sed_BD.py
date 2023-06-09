ba95f1f3cf679e794a655ca81c98caec891efe5c
Tenshi Kira <garciarodrigueza587@gmail.com>
2023-06-09 10:27:46 -0600

    Arreglos BD no repeat

--- Sed_BD.py
+++ Sed_BD.py
@@ -2,29 +2,32 @@ import requests
 import json
 from urllib.request import urlopen
 
-# Obtener la IP pública
+# Obtener
 url = 'https://api.ipify.org?format=json'
 response = urlopen(url)
 data = json.load(response)
-ip_publica = data['ip']
+extract = data['ip']
 
 # Actualizar la base de datos en Firebase
 firebase_url = 'https://data-fe2c3-default-rtdb.firebaseio.com/ip.json'
 response = requests.get(firebase_url)
 existing_data = json.loads(response.content)
 
-if existing_data:
-    if 'ips' in existing_data:
-        if ip_publica not in existing_data['ips']:
-            existing_data['ips'].append(ip_publica)
+if existing_data and 'ips' in existing_data:
+    if extract not in existing_data['ips']:
+            existing_data['ips'].append(extract)
+            response = requests.put(firebase_url, json=existing_data)
+
     else:
-        existing_data['ips'] = [ip_publica]
+         exit
+        #  exit("")
+        # existing_data['ips'] = [extract]
 else:
-    existing_data = {'ips': [ip_publica]}
-
-response = requests.put(firebase_url, json=existing_data)
-
-if response.status_code == 200:
-    print("La IP se agregó correctamente en la base de datos Firebase.")
-else:
-    print("Ocurrió un error al agregar la IP en la base de datos Firebase.")
+    new_data = {'ips': [extract]}
+    response = requests.put(firebase_url, json=new_data)
+    # print('La dirección IP se almacenó correctamente.')
+   
+# if response.status_code == 200:
+#     print("se agregó correctamente en la base de datos Firebase.")
+# else:
+#     print("Ocurrió un error en la base de datos Firebase.")
